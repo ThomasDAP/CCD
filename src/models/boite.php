cec6c288056e3c6a84a33f626394da9d169ad726
ThomasDAP <95960414+ThomasDAP@users.noreply.github.com>
2022-03-10 10:13:08 +0100

    iodjg

---
+++ src/models/boite.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace custumbox\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class boite extends Model
+{
+    protected $table = "boite";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+}
