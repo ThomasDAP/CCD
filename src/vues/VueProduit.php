add7525da603ae33651d8a8328cf09ce303d8a54
ThomasDAP <95960414+ThomasDAP@users.noreply.github.com>
2022-03-10 14:16:58 +0100

    Siuuuuuu

--- src/vues/VueProduit.php
+++ src/vues/VueProduit.php
@@ -38,7 +38,7 @@ class VueProduit
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                 <h6>{$prod['description']}</h6>
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="">View options</a></div>
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="" id="ajoutPanier">Ajouter à la boite</a></div>
                             </div>
                         </div>
                     </div>
@@ -55,25 +55,26 @@ END;
     public function getHeader(){
         return <<<END
     <!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Shop Homepage - Start Bootstrap Template</title>
+        <title>L'Atelier 17.91</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="../../Documents/Template/css/styles.css" rel="stylesheet" />
+        <link href="../../Documents/Template/css/stylePerso.css" rel="stylesheet" />
     </head>
     <body>
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <a class="navbar-brand" href="#!">L'atelier 17.91</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -89,13 +90,13 @@ END;
                             </ul>
                         </li>
                     </ul>
-                    <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
+
+                        <button class="btn btn-outline-dark" id="button_cart" >
                             <i class="bi-cart-fill me-1"></i>
-                            Cart
+                            Panier
                             <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
                         </button>
-                    </form>
+
                 </div>
             </div>
         </nav>
@@ -103,16 +104,20 @@ END;
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <!-- <h1 class="display-4 fw-bolder">Shop in style</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p> -->
+                    <div class="titre_page"><p>L'Atelier : créateur de lien</p></div>
+                    <img src="../../Documents/Template/assets/logo.png" height="100px" width="100px"/>
+                    <div class="desc_page"><p>Gaëlle et Mégane, deux jeunes mosellanes se mobilisent pour lutter contre l’isolement, l’exclusion
+                        et la précarité en proposant des solutions créatives et solidaires.
+                    </p></div>
                 </div>
             </div>
         </header>
         <!-- Section-->
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5" id="AfficheurProduits">
-            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-               
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
 END;
 
     }
