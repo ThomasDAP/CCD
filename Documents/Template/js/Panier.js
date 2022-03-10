dc938ba748b5275f2a30770c7391c21fa169d7b6
ThomasDAP <95960414+ThomasDAP@users.noreply.github.com>
2022-03-10 10:37:14 +0100

    Bonsoir Corentin

---
+++ Documents/Template/js/Panier.js
@@ -0,0 +1,36 @@
+let tab = [];
+
+function ajouterPanier(prod){
+
+        console.log("Ajout au panier de : " + prod.nom);
+        let objTrouve = tab.find(e => e.produit.nom === prod.nom);
+
+        if (objTrouve) {
+            let idxObj = tab.indexOf(objTrouve);
+            tab[idxObj].quantite++;
+        } else {
+
+            let obj = {
+                produit: prod,
+                quantite: 1
+            }
+            tab.push(obj);
+
+        }
+
+
+        return tab;
+
+
+}
+
+function afficherPanier() {
+    
+}
+
+
+
+export default {
+    tab,
+    ajouterPanier
+}
